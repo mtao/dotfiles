9d9b04c5890176a3cc28280aa92d5834c38e1fea
Michael Tao <michael.tao@nyu.edu>
2026-01-03 15:25:36 -0500

    warning cleaning on a py file

--- update_configs.py
+++ update_configs.py
@@ -2,8 +2,9 @@
 
 import sys
 import os
-import shutil
 import difflib
+from typing import override
+from pathlib import Path
 
 mypath = os.path.dirname(os.path.realpath(__file__))
 default_filename = os.path.join(mypath,"settings.txt")
@@ -11,31 +12,32 @@ default_configdir = os.path.join(mypath,"configs")
 
 
 class Command:
-    update_target = 0
-    update_source = 1
-    update_noop= 2
-    __update_readable__ = {update_target: "Target",
+    update_target: int = 0
+    update_source: int = 1
+    update_noop: int = 2
+    __update_readable__: dict[int,str] = {update_target: "Target",
             update_source: "Source",
             update_noop: "NoOp"}
 
-    def __init__(self, target):
-        self.__target__ = target
+    def __init__(self, target: Path):
+        self.__target__: Path = target
 
-    def __type_str__(self):
+    def __type_str__(self) -> str:
         raise NotImplementedError
 
-    def diff(self):
+    def diff(self) -> list[str]:
         raise NotImplementedError
 
-    def __get_file_age__(filename):
+    @staticmethod
+    def __get_file_age__(filename: Path):
         st = os.stat(os.path.expanduser(filename))
         return st.st_mtime
 
 
-    def target_age(self):
+    def target_age(self) -> float:
         return Command.__get_file_age__(self.__target__)
 
-    def source_age(self):
+    def source_age(self) -> float:
         raise NotImplementedError
 
     def target(self):
@@ -44,9 +46,9 @@ class Command:
     def source(self):
         return NotImplementedError
 
-    def update_style(self):
+    def update_style(self) -> int:
         if len(self.diff()) == 0:
-            return self.update_noop
+            return Command.update_noop
         ta = self.target_age()
         na = self.source_age()
 
@@ -59,60 +61,66 @@ class Command:
     def update_human_readable(self):
         return Command.__update_readable__[self.update_style()]
 
-
-    def __repr__(self):
+    @override
+    def __repr__(self) -> str:
         return "{0}:[{1}],{2}".format(self.__type_str__(),self.__target__,self.update_human_readable())
 
 
 class FileCommand(Command):
+    @override
     def __type_str__(self):
         return "File"
-    def __init__(self,target,args):
+    def __init__(self,target: Path,args: list[str]):
         super().__init__(target)
-        self.__source__ = os.path.join(default_configdir,args[0])
+        self.__source__: Path = Path(os.path.join(default_configdir,args[0]))
         #TODO: this only works for text files I think?
         source_lines = open(os.path.expanduser(self.__source__),"r").readlines()
         target_lines = open(os.path.expanduser(self.__target__),"r").readlines()
-        self.__diff__ = list(difflib.unified_diff(source_lines,target_lines))
+        self.__diff__: list[str] = list(difflib.unified_diff(source_lines,target_lines))
 
+    @override
     def diff(self):
         return self.__diff__
 
 
+    @override
     def source_age(self):
         return Command.__get_file_age__(self.__source__)
 
 
 class GitCommand(Command):
-    def __type_str__(self):
+    @override
+    def __type_str__(self) -> str:
         return "Git"
-    def diff(self):
+    @override
+    def diff(self) -> list[str]:
         return []
-    def __init__(self,target,args):
+    def __init__(self,target: Path):
         super().__init__(target)
 
+    @override
     def source_age(self):
         return -1
 
 
-def make_command(cmd,target,args):
+def make_command(cmd: str,target: Path,args: list[str]):
     if cmd == "=":
         return FileCommand(target,args)
     elif cmd == ":git:":
-        return GitCommand(target,args)
+        return GitCommand(target)
     else:
-        raise Exception("Unknown Command",cmd)
+        raise Exception("Unknown Command {} {} [{}]".format(cmd,target,args))
 
 
 
 
 class Settings:
-    def __init__(self, lines):
-        self.__name__ = lines[0][1:-1]
+    def __init__(self, lines: list[str]):
+        self.__name__: str = lines[0][1:-1]
         print(self.__name__)
         
         #triplets of command, target, command args
-        self.__commands__ = [make_command(line[1],line[0],line[2:]) for line in map(lambda x: x.split(), lines[1:])]
+        self.__commands__: list[Command] = [make_command(line[1],Path(line[0]),line[2:]) for line in map(lambda x: x.split(), lines[1:])]
 
     def process(self):
         for cmd in self.__commands__:
@@ -129,11 +137,11 @@ class Settings:
 def __main__():
     filename = default_filename
     if len(sys.argv) > 1:
-        filename = argv[1]
+        filename = sys.argv[1]
     with open(filename,"r") as f:
         lines = list(filter(lambda x: len(x) > 0,map(lambda x: x.strip(),  f.readlines())))
         print(lines)
-        indices = []
+        indices: list[int] = []
 
         for i,line in enumerate(lines):
             if line[0] == '[':
